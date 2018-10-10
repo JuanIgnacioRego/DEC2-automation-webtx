0be4ca8cca8b77a42410af71b3f78e4a0867463b
Juan Ignacio Rego <JuanIgnacio.Rego@gmail.com>
2018-10-10 09:04:31 -0300

    Actualizado backup_build.sh, tag 'latest2' ya que 'latest' quedó trabado en la registry

--- backup_build.sh
+++ backup_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMG="lapp-dvde004:5000/webtxtest:$(git describe --abbrev=0 --tags 2>/dev/null || echo 'latest')"
+IMG="lapp-dvde004:5000/webtxtest:latest2"
 
 echo "Building $IMG"
 docker build -t "$IMG" . && \
